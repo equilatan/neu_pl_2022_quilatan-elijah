cdb769bccbbf4b05a04d84b23aeacff36cf2800f
Giruthanda <jojomemeadvent@gmail.com>
2022-09-10 22:49:44 +0800

    revised Lab 2

--- lab_activities/LabAct_2.java
+++ lab_activities/LabAct_2.java
@@ -1,37 +1,69 @@
+import java.util.Scanner;
+
 public class LabAct_2 {
 
     public static void main(String args[]){
+        String input;
+        boolean accepted;
 
+        do{
+            input = getString("Input String: ");
+        }while(processDFA(input)==false);
+    }
+
+    static boolean processDFA(String input){
         String state = "q0";
-        String input = "01111110101010100001";
 
         for (int i = 0; i < input.length(); i++){
-        //q0
+            //q0
             if(state.equals("q0") && input.charAt(i)=='0')
                 state="q1";
             else if(state.equals("q0") && input.charAt(i)=='1')
                 state="q0";
-        //q1
+            //q1
             else if(state.equals("q1") && input.charAt(i)=='0')
                 state="q1";
             else if(state.equals("q1") && input.charAt(i)=='1')
                 state="q2";
-        //q2
+            //q2
             else if(state.equals("q2") && input.charAt(i)=='0')
                 state="q1";
             else if(state.equals("q2") && input.charAt(i)=='1')
-                state="q2";
-        //invalid input
+                state="q0";
+            //invalid input
             else{
-                System.out.println("Invalid Input. Exiting automaton...");
-                break;
+                System.out.println("Invalid Input.");
+                tryAgain();
+                return false;
             }
 
         }
 
-        if(state.equals("q2"))
+        if(state.equals("q2")){
             System.out.println("String accepted");
-        else
-            System.out.println("String not accepted.");
+            return true;
+        }
+        else {
+            System.out.println("String not accepted. ");
+            tryAgain();
+            return false;
+        }
+
+    }
+
+    static void tryAgain(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Would you like to input another string? [y/n]: ");
+        char yesOrNo = scan.next().charAt(0);
+        switch(yesOrNo){
+            case 'y' : System.out.println(); break;
+            case 'n' : System.exit(0); break;
+            default :System.exit(0); break;
+        }
+    }
+
+    static String getString(String s) {
+        System.out.print(s);
+        return new Scanner(System.in).nextLine();
     }
 }
